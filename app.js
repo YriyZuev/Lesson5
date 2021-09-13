bc703074214fc3f9c9060e3cfddb5e949177ac1f
Yriy <90506298+YriyZuev@users.noreply.github.com>
2021-09-13 22:00:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timeEl = document.querySelector('#time')
+const board = document.querySelector('#board')
+let time = 0
+let score = 0
+const colors = ['#cc232ee5', '#cc2394e5', '#761485e5', '#3c18c9e5', '#14bde3e5', '#1fdbc5e5', '#21d933e5', '#cce815e5', '#d9870de5', '#c43616e5']
+
+startBtn.addEventListener('click', (event) => {
+    event.preventDefault() // Отменяет поведение ссылки по дефолту
+    screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', event => {
+    if (event.target.classList.contains('time-btn')) {
+        time = parseInt(event.target.getAttribute('data-time')) // parseInt для перевода строки в число
+        screens[1].classList.add('up')
+        startGame()
+    }
+})
+
+board.addEventListener('click', event => {
+    if(event.target.classList.contains('circle')) {
+        score++
+        event.target.remove()
+        createRandomCircle()
+    }
+})
+
+function startGame() {
+    setInterval(decreaseTime, 1000)
+    createRandomCircle()
+    setTime(time)
+}
+
+function decreaseTime() {
+    if (time === 0) {
+        finishGame()
+    } else {
+        let current = --time
+        if (current < 10) {
+        current = `0${current}`
+    }
+        setTime(current)
+    }
+}
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+    timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1>`
+}
+
+function createRandomCircle() {
+    const circle = document.createElement('div')
+    const size = getRandomNumber(10, 60)
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size) // Вычитаем size чтобы не выходило за поле
+    const y = getRandomNumber(0, height - size)
+    const color = getRandomColor()
+
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+    circle.style.background = `${color}`
+    circle.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+    
+    board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+     return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
